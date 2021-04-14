69a001cb12bac4fcf85730b02a7a17d8a7d89b0a
rollercoasterfinance <info@rollercoaster.finance>
2021-04-14 21:03:37 +0200

    reverts staging to mainnet network

--- src/utils/networks.ts
+++ src/utils/networks.ts
@@ -4,9 +4,9 @@ import { ChainId } from '@pancakeswap-libs/sdk';
 export const getIsNetworkSupported = (chainId: number) => {
     switch (process.env.NEXT_PUBLIC_ENVIRONMENT) {
         case NextEnvironment.DEVELOPMENT:
-        case NextEnvironment.STAGING:
             return chainId === ChainId.BSCTESTNET;
 
+        case NextEnvironment.STAGING:
         case NextEnvironment.PRODUCTION:
             return chainId === ChainId.MAINNET;
 
